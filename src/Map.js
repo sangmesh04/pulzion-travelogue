2c33fbf35abbd03317899d0d336979959a0db108
sangmesh04 <78370367+sangmesh04@users.noreply.github.com>
2022-04-23 19:13:23 +0530

    map issue in y-axis

--- src/Map.js
+++ src/Map.js
@@ -106,7 +106,7 @@ const MapT = () => {
    </div>
  </nav>
 
- <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4"  style={{zIndex:'-1'}}>
+ <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4" >
    <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
      <h1 className="h2">Map</h1>
      <div className="btn-toolbar mb-2 mb-md-0">
